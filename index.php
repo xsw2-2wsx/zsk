9c22eba73a334ebb6829f5d53973201bd3394b01
xsw2-2wsx <jan1undrych@gmail.com>
2021-10-05 12:50:35 +0200

    feat: crete form

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <form action="submit.php" method="POST">
+        <h3>Podaj ilosc osob w rodzinie</h3>
+        <input type="text" name="amount" placeholder="Ilosc osob">
+        <input type="submit" value="zatwierdz">
+    </form>
+</body>
+</html>
