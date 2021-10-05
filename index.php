89e5378f00bc4e0cc0bbf9f0a6c3c8695ca27845
xsw2-2wsx <jan1undrych@gmail.com>
2021-10-05 13:34:56 +0200

    feat: complete task

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title></title>
 </head>
 <body>
-    <?php if(!$_POST['amount']): ?>
+    <?php if(!$_POST['amount'] and !$_POST['age1']): ?>
     <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
         <h3>Podaj ilosc osob w rodzinie</h3>
         <input type="text" name="amount" placeholder="Ilosc osob">
@@ -15,11 +15,46 @@
     </form>
     <?php endif; ?>
 
-    <?php if($_POST['amount']): ?>
-    <span>Ilosc osob: <?php echo $_POST['amount'] ?></span>
-    </br>
-    <a href="<?php $_SERVER['PHP_SELF']?>">Powrot</a>
-    <?php endif; ?>
+    <?php 
+        if($_POST['amount'] and !$_POST['age1']) {
+            $addr = $_SERVER['PHP_SELF'];
+            echo <<< END
+            <form action="$addr" method="POST">
+            END;
+            for($i = 1; $i <= (int) $_POST['amount']; $i++) {
+                echo <<< END
+                <p>Wiek osoby $i: </p>
+                <input type="number" required placeholder="wiek" name="age$i">
+                END;
+            }
+            echo '</br></br><button type="submit">Zatwierdz</button></form>';
+        }
+    ?>
+
+    <?php 
+        if($_POST['age1']) {
+            $sum = 0;
+            $i = 1;
+
+            while($_POST['age'.$i]) {
+                $sum += (int) $_POST['age'.$i];
+                $i++;
+            }
+
+            $avg = $sum / $i;
+
+            $addr = $_SERVER["PHP_SELF"];
+            echo <<< END
+                Suma wieku: $sum </br>
+                Ilosc osob: $i </br>
+                Sredni wiek: $avg </br>
+                <a href="$addr">Powrot</a>
+            END;
+            
+        }
+    ?>
 </body>
+
+
 </html>
 
